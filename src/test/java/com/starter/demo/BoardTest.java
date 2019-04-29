962bbc21d6ddadd1ecc03a14f0c7986b604b709c
guillaume.pasquier <guillaume.pasquier@gfi.fr>
2019-04-29 10:05:40 +0200

    junit 4.12 + tests

---
+++ src/test/java/com/starter/demo/BoardTest.java
@@ -0,0 +1,40 @@
+package com.starter.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author frup70473
+ *
+ */
+public class BoardTest {
+
+    BoardService serv = null;
+
+    @Before
+    public void before() {
+        serv = new BoardService();
+    }
+
+    @Test
+    public void test_make_zero() {
+        int nbOf = 0;
+        assertTrue(serv.makeBoards(nbOf).size() == nbOf);
+    }
+
+    @Test
+    public void test_make_one() {
+        int nbOf = 1;
+        assertTrue(serv.makeBoards(nbOf).size() == nbOf);
+    }
+
+    @Test
+    public void test_make_correct_type() {
+        int nbOf = 1;
+        assertTrue(serv.makeBoards(nbOf).get(0) instanceof Board);
+    }
+
+}
